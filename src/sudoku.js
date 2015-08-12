9095f20949a4ee455d1e740a24d25c730b6886d5
Amalia Viti <amalia.viti@gmail.com>
2015-08-12 12:18:18 -0400

    adds current game state and validators, no longer checks against solution on input

--- src/sudoku.js
+++ src/sudoku.js
@@ -3,6 +3,9 @@ var $ = require('jquery');
 var Sudoku = module.exports = function (boardId) {
 
   this.$board = $('#' + boardId);
+
+  // hard-coded game and solution
+  // TODO a generator should generate these 2 arrays
   this.game = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -27,6 +30,8 @@ var Sudoku = module.exports = function (boardId) {
     [3, 4, 5, 2, 8, 6, 1, 7, 9]
   ];
 
+  this.currentGame = null;
+
   this.createInput = function () {
     var $input = $(document.createElement('input'));
     $input.attr('type', 'text');
@@ -49,6 +54,9 @@ Sudoku.prototype = {
     var $board = this.$board;
     var squares = [ [], [], [] ];
 
+    // clone game to current game
+    this.setCurrentGame();
+
     for (i = 0; i < 9; i++) {
       for (j = 0; j < 9; j++) {
         k = Math.floor( i / 3);
@@ -58,7 +66,7 @@ Sudoku.prototype = {
           squares[k][l] = $(document.createElement('div')).addClass('square');
         }
 
-        value = this.game[i][j];
+        value = this.currentGame[i][j];
         $input = this.createInput();
 
         if (value === 0) {
@@ -88,37 +96,173 @@ Sudoku.prototype = {
     this.setupEventListeners();
   },
 
+  /**
+   * clones game board to store state
+   */
+  setCurrentGame: function () {
+     this.currentGame = this.game.slice(0);
+  },
+
+  /**
+   * sets up input event listeners
+   */
   setupEventListeners: function () {
     var self = this;
 
     $('.editable input').on('keyup', function (e) {
-      self.checkAnswer(e.currentTarget);
+      self.checkInput(e.currentTarget);
     });
 
   },
 
-  checkAnswer: function (target) {
+  /**
+   * gets cell value from current game board
+   * @param  int row  row index
+   * @param  int col  column index
+   * @return int      cell value
+   */
+  getCellValue: function (row, col) {
+    return this.getCurrentGame[row][col];
+  },
+
+  /**
+   * sets cell value on current game board
+   * @param int inputVal  input value
+   * @param int row       row index
+   * @param int col       column index
+   */
+  setCellValue: function (inputVal, row, col) {
+    this.currentGame[row][col] = inputVal;
+  },
+
+  /**
+   * gets the square location of a cell
+   * (there are 9, and each "square" contains 9 cells)
+   * @param  int row  row index
+   * @param  int col  column index
+   * @return object   object with square indexes (i.e. { row: 0, col: 0 })
+   */
+  getSquare: function (row, col) {
+    return {
+      row: Math.floor(row / 3) * 3,
+      col: Math.floor(col / 3) * 3
+    };
+  },
+
+  /**
+   * validates if value can be placed in a row
+   * (i.e. if value does not already exist in the row)
+   * @param  int inputVal input value
+   * @param  int row      row index
+   * @return bool         true for valid, false for invalid
+   */
+  checkRow: function (inputVal, row) {
+
+    var i;
+
+    for (i = 0; i < 9; i++) {
+      if (inputVal === this.currentGame[row][i]) {
+        return false;
+      }
+    }
+
+    return true;
+
+  },
+
+  /**
+   * validates if value can be placed in a column
+   * @param  int inputVal input value
+   * @param  int square   column index
+   * @return bool         true for valid, false for invalid
+   */
+  checkColumn: function (inputVal, col) {
+
+    var i;
+
+    for (i = 0; i < 9; i++) {
+      if (inputVal === this.currentGame[i][col]) {
+        return false
+      }
+    }
+
+    return true;
+
+  },
+
+  /**
+   * checks if value can be placed in a square
+   * @param  int inputVal input value
+   * @param  int row      row index
+   * @param  int col      column index
+   * @return bool         true for valid, false for invalid
+   */
+  checkSquare: function (inputVal, row, col) {
+
+    var i, j;
+    var square = this.getSquare(row, col);
+
+    for (i = square.row; i < square.row + 3; i++) {
+      for (j = square.col; j < square.col + 3; j++) {
+        if (inputVal === this.currentGame[i][j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+
+  },
+
+  /**
+   * checks validity of cell input
+   * @param  element target the changed input
+   */
+  checkInput: function (target) {
     var val = parseInt(target.value, 10);
     var $cell = $(target).closest('.cell');
     var row = $cell.data('row');
     var col = $cell.data('col');
 
     // validate and check answer to solution board
-    if (/^[1-9]$/.test(val) && val === this.gameSolution[row][col]) {
+    if (
+      /^[1-9]$/.test(val) &&
+      this.checkRow(val, row) &&
+      this.checkColumn(val, col) &&
+      this.checkSquare(val, row, col)
+    ) {
 
-      $cell.html('');
-      $cell.text(val)
-        .removeClass('editable')
-        .removeClass('wrong')
-        .addClass('verified');
+      // if answer is validated, set cell value
+      this.setCellValue(val, row, col);
 
     } else {
-      this.clearCell($cell);
+
+      // set cell value to 0
+      this.setCellValue(0, row, col);
+
+      // clear cell input
+      this.clearInput($cell);
+
     }
 
   },
 
-  clearCell: function ($cell) {
+  /**
+   * checks answer against game solution board
+   * @param  int val inputted value
+   * @param  int row row index
+   * @param  int col column index
+   * @return bool    true if correct, false if incorrect
+   */
+  checkAnswer: function (val, row, col) {
+    return (val === this.gameSolution[row][col]);
+  },
+
+  /**
+   * clears cell's input element
+   * @param  element $cell  cell element
+   */
+  clearInput: function ($cell) {
     $cell
       .addClass('wrong')
       .delay(1000)
